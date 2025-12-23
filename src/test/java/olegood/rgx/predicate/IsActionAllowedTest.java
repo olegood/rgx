40f47237feaa1dbfca40cbfdb39cbb1581d410a3
Oleg Anastassov <oleg.anastassov@gmail.com>
2025-12-23 17:29:10 +0100

    style: apply spotless formatting

--- src/test/java/olegood/rgx/predicate/IsActionAllowedTest.java
+++ src/test/java/olegood/rgx/predicate/IsActionAllowedTest.java
@@ -1,45 +1,43 @@
 package olegood.rgx.predicate;
 
-import olegood.rgx.domain.document.Document;
-import olegood.rgx.domain.document.DocumentStatus;
-import org.junit.jupiter.api.Test;
-
 import static olegood.rgx.domain.document.DocumentAction.APPROVE;
 import static olegood.rgx.domain.document.DocumentAction.SUBMIT;
 import static olegood.rgx.domain.document.DocumentStatus.DRAFT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import olegood.rgx.domain.document.Document;
+import org.junit.jupiter.api.Test;
+
 class IsActionAllowedTest {
-    
-    @Test
-    void shouldReturnTrueIfActionAllowed() {
-        // when
-        var document = new Document().setStatus(DRAFT);
 
-        // then
-        assertThat(new IsActionAllowed(SUBMIT).test(document)).isTrue();
-    }
+  @Test
+  void shouldReturnTrueIfActionAllowed() {
+    // when
+    var document = new Document().setStatus(DRAFT);
 
-    @Test
-    void shouldReturnFalseIfActionIsNotAllowed() {
-        // when
-        Document document = new Document().setStatus(DRAFT);
+    // then
+    assertThat(new IsActionAllowed(SUBMIT).test(document)).isTrue();
+  }
 
-        // then
-        assertThat(new IsActionAllowed(APPROVE).test(document)).isFalse();
-    }
+  @Test
+  void shouldReturnFalseIfActionIsNotAllowed() {
+    // when
+    Document document = new Document().setStatus(DRAFT);
 
-    @Test
-    void shouldReturnFalseIfDocumentIsNull() {
-        // expect
-        assertFalse(new IsActionAllowed(APPROVE).test(null));
-    }
+    // then
+    assertThat(new IsActionAllowed(APPROVE).test(document)).isFalse();
+  }
 
-    @Test
-    void shouldReturnFalseIfDocumentStatusIsNull() {
-        // expect
-        assertFalse(new IsActionAllowed(SUBMIT).test(new Document().setStatus(null)));
-    }
-    
+  @Test
+  void shouldReturnFalseIfDocumentIsNull() {
+    // expect
+    assertFalse(new IsActionAllowed(APPROVE).test(null));
+  }
+
+  @Test
+  void shouldReturnFalseIfDocumentStatusIsNull() {
+    // expect
+    assertFalse(new IsActionAllowed(SUBMIT).test(new Document().setStatus(null)));
+  }
 }
