8811cc695e3394d06c6b5169f3472b4a44e3a082
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-01-10 23:46:36 +0100

    test: enhance operation execution verification

--- src/test/java/olegood/rgx/service/document/DocumentStatusHandlerTest.java
+++ src/test/java/olegood/rgx/service/document/DocumentStatusHandlerTest.java
@@ -1,17 +1,19 @@
 package olegood.rgx.service.document;
 
+import static java.util.Collections.emptySet;
 import static olegood.rgx.domain.document.DocumentAction.REOPEN;
+import static olegood.rgx.domain.document.DocumentAction.SUBMIT;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 import java.util.Set;
 import olegood.rgx.domain.document.Document;
 import olegood.rgx.domain.document.DocumentRepository;
-import olegood.rgx.service.document.status.Operation;
+import olegood.rgx.service.document.status.impl.Submit;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -20,21 +22,36 @@ class DocumentStatusHandlerTest {
 
   private static final Long ID = 42L;
 
-  @Mock private Set<Operation> operations;
-
+  @Mock private Submit submit;
   @Mock private DocumentRepository documentRepository;
-
-  @InjectMocks private DocumentStatusHandler documentStatusHandler;
 
   @Test
   void shouldThrowExceptionForUnknownAction() {
     // when
-    var document = new Document();
+    var document = new Document().setId(ID);
     when(documentRepository.findById(ID)).thenReturn(Optional.of(document));
 
     // then
     assertThatExceptionOfType(UnsupportedOperationException.class)
-        .isThrownBy(() -> documentStatusHandler.handleAction(ID, REOPEN))
+        .isThrownBy(
+            () ->
+                new DocumentStatusHandler(emptySet(), documentRepository).handleAction(ID, REOPEN))
         .withMessage("Unknown action: REOPEN");
+  }
+
+  @Test
+  void shouldExecuteOperationIfFound() {
+    // given
+    var document = new Document().setId(ID);
+    when(documentRepository.findById(ID)).thenReturn(Optional.of(document));
+
+    when(submit.associatedAction()).thenCallRealMethod();
+    when(submit.isEligible()).thenReturn(doc -> true);
+
+    // when
+    new DocumentStatusHandler(Set.of(submit), documentRepository).handleAction(ID, SUBMIT);
+
+    // then
+    verify(submit).execute(document);
   }
 }
