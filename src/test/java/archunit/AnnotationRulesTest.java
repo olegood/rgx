142c0d0297c48d651e8aecb8dcb433b18a071092
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-01-15 22:58:23 +0100

    test: freeze archrule for jpa entities to include table annotation

--- src/test/java/archunit/AnnotationRulesTest.java
+++ src/test/java/archunit/AnnotationRulesTest.java
@@ -1,6 +1,7 @@
 package archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -13,10 +14,11 @@ class AnnotationRulesTest {
 
   @ArchTest
   static final ArchRule entities_must_have_table_annotation =
-      classes()
-          .that()
-          .areAnnotatedWith(Entity.class)
-          .should()
-          .beAnnotatedWith(Table.class)
-          .because("JPA entities must explicitly declare table mapping");
+      freeze(
+          classes()
+              .that()
+              .areAnnotatedWith(Entity.class)
+              .should()
+              .beAnnotatedWith(Table.class)
+              .because("JPA entities must explicitly declare table mapping"));
 }
