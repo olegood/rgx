83d5286957afb278bf7a53c548e2877d60fdc57e
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-01-18 23:03:56 +0100

    test: de-freeze the rule enforcing assertj usage

--- src/test/java/archunit/TestStructureRulesTest.java
+++ src/test/java/archunit/TestStructureRulesTest.java
@@ -1,7 +1,6 @@
 package archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -12,11 +11,9 @@ class TestStructureRulesTest {
 
   @ArchTest
   static final ArchRule assertions_should_use_assertj_and_not_junit_jupiter =
-      freeze(
-          noClasses()
-              .should()
-              .dependOnClassesThat()
-              .haveFullyQualifiedName("org.junit.jupiter.api.Assertions")
-              .because(
-                  "AssertJ provides better readability and more powerful assertion capabilities."));
+      noClasses()
+          .should()
+          .dependOnClassesThat()
+          .haveFullyQualifiedName("org.junit.jupiter.api.Assertions")
+          .because("AssertJ provides better readability and more powerful assertion capabilities.");
 }
