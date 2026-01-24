d2550354292cca83d56d206fe79fdf106c0b4c13
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-01-24 01:20:51 +0100

    refactor: move violation class to engine package

--- src/main/java/olegood/rgx/validation/constraint/Violation.java
+++
@@ -1,7 +0,0 @@
-package olegood.rgx.validation.constraint;
-
-import lombok.Builder;
-
-@Builder
-public record Violation(String message) {
-}
---
+++ src/main/java/olegood/rgx/validation/engine/Violation.java
@@ -0,0 +1,6 @@
+package olegood.rgx.validation.engine;
+
+import lombok.Builder;
+
+@Builder
+public record Violation(String message) {}
