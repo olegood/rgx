80b62d705ba8c5c2d5957f8a0b3ab0ed4488b046
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-03-13 20:36:38 +0100

    docs: add javadocs for organization mapper methods

--- src/main/java/olegood/rgx/mapper/OrganizationMapper.java
+++ src/main/java/olegood/rgx/mapper/OrganizationMapper.java
@@ -4,10 +4,27 @@ import olegood.rgx.domain.Organization;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
+/**
+ * A mapper interface for converting between {@link Organization} entities and {@link
+ * OrganizationData} records. This interface provides methods to map entities to records and vice
+ * versa, enabling a convenient transformation layer for data transfer objects.
+ */
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OrganizationMapper {
 
+  /**
+   * Maps an {@link Organization} entity to an {@link OrganizationData} record.
+   *
+   * @param organization the {@link Organization} entity to be transformed
+   * @return an {@link OrganizationData} record representing the provided entity
+   */
   OrganizationData toResponse(Organization organization);
 
+  /**
+   * Converts an {@link OrganizationData} record into an {@link Organization} entity.
+   *
+   * @param organizationData the {@link OrganizationData} record to be transformed
+   * @return an {@link Organization} entity representing the provided record
+   */
   Organization toEntity(OrganizationData organizationData);
 }
